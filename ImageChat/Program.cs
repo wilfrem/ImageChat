31f58802ccb814afd37cee0ccf5cfc0b145a646b
Kazuki Yasufuku <wilfrem@gmail.com>
2014-02-22 22:25:57 +0900

    とりあえず、Owinを動かすところまで

---
+++ ImageChat/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Daemonizer;
+using Microsoft.Owin.Hosting;
+
+namespace ImageChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port;
+            if (args.Length == 0 || int.TryParse(args[0], out port))
+            {
+                port = 12121;
+            }
+#if DEBUG
+            var url = "http://localhost:" + port;
+#else
+            var url = "http://+:" + port;
+#endif
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Server started at " + port);
+                try
+                {
+                    Daemon.Instance.WaitForUnixSignal();
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("not in *nix platform. wait using Console.ReadLine");
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
